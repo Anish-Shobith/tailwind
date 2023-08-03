36b26f4fdaa8c78f5109b404fecb244147637a1a
Anish-Shobith <anish.shobith19@gmail.com>
2023-08-03 18:35:22 +0000

    build: tsc build for 3614aad33427f84b70d59b9ac96dcd9218e6a7a6

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,8 @@
-import type { Config } from "tailwindcss";
 //@ts-ignore
 import animatePlugin from "tailwindcss-animate";
-
 import { anishPlugin } from "./plugin";
-
 export const anishPreset = {
     darkMode: ["class"],
     content: [],
     plugins: [animatePlugin, anishPlugin],
-} satisfies Config;
-
+};
